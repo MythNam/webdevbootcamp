84478695e81f4ed8c11785f4c9c0f1fa73b76260
Son Dang <son.th.dang@gmail.com>
2018-04-17 05:58:00 +0000

    A first complete Express App using horizontal file structure

--- ExpressAppDemo/app/routes/index.server.routes.js
+++ ExpressAppDemo/app/routes/index.server.routes.js
@@ -1,6 +1,6 @@
 module.exports = function(app) {
     var index = require("../controllers/index.server.controller.js");
-    app.get("/", index.render());
+    app.get("/", index.render);
 };
 
 //app.route("/").get( function(){ res.send("This is a GET request"); 
--- ExpressAppDemo/server.js
+++ ExpressAppDemo/server.js
@@ -1,7 +1,11 @@
-var express = require("./config/express");
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+var express = require("./config/express");
 var app = express();
 
 app.listen( process.env.PORT, process.env.IP, function() {
     console.log("Server started to listen to PORT 3000!");
 });
+
+module.exports = app;
+console.log('Server running at http://localhost:3000/');
